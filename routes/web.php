b763214770ee8a928e7037df9b6fdc6044aba25c
ahmadaait <jamezmada@gmail.com>
2023-03-26 21:39:37 +0700

    chore: clean route

--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-// Route::get('/login', function () {
-//     return view('login');
-// });
-// Route::get('/signup', function () {
-//     return view('signup');
-// });
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-Route::get('/login',[AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'authenticating'])->middleware('guest');
 Route::get('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', [dashboardController::class,'dashboard']);
+    Route::get('/dashboard', [dashboardController::class, 'dashboard']);
 });
-
-
-
-
