f943b8f46dcef125ea76bd96dcaefd90aec6922f
Yonanda <yonandaharyono@gmail.com>
2023-04-21 14:23:43 +0700

    Validation Login Page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,46 +11,59 @@
     <div class="min-h-screen flex items-center justify-center">
         <div class="bg-white p-8 rounded-lg shadow-lg max-w-sm w-full">
             <h1 class="text-2xl font-bold text-gray-700 mb-6 text-center">Login</h1>
-            @if (Session::has('status'))
-            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <strong class="font-bold">{{ Session::get('message')}}</strong>
-                {{-- <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20">
-                        <title>Close</title>
-                        <path
-                            d="M14.348 5.652a1 1 0 00-1.414 0L10 8.586 6.066 4.652a1 1 0 00-1.414 1.414L8.586 10l-3.934 3.934a1 1 0 001.414 1.414L10 11.414l3.934 3.934a1 1 0 001.414-1.414L11.414 10l3.934-3.934a1 1 0 000-1.414z" />
-                    </svg>
-                </span> --}}
-            </div>
-            @endif
             <form method="POST" action="">
                 @csrf
                 <div class="mb-4 mt-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="email">
+                    <label
+                        class="block text-gray-700 font-bold mb-2{{ $errors->has('email') ? 'block mb-2 text-sm font-bold text-red-700 dark:text-red-500' : '' }}"
+                        for="email">
                         Email
                     </label>
-                    <input type="email" class="border border-gray-400 p-2 w-full rounded-md" name="email"
-                        placeholder="Enter your email" />
+                    <input type="text"
+                        class="border border-gray-400 p-2 w-full rounded-md {{ $errors->has('email') ? ' border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500' : '' }}"
+                        name="email" placeholder="Enter your email" />
+                    @if ($errors->has('email'))
+                        <p class="text-sm text-red-600 dark:text-red-500"><span
+                                class="font-medium">{{ $errors->first('email') }}</span></p>
+                    @endif
                 </div>
                 <div class="mb-4">
-                    <label class="block text-gray-700 font-bold mb-2" for="password">
+                    <label
+                        class="block text-gray-700 font-bold mb-2{{ $errors->has('password') ? 'block mb-2 text-sm font-bold text-red-700 dark:text-red-500' : '' }}"
+                        for="password">
                         Password
                     </label>
-                    <input class="border border-gray-400 p-2 w-full rounded-md" name="password" type="password"
-                        placeholder="Enter your password" />
+                    <div class="relative flex items-center text-gray-400 focus-within:text-gray-600">
+                        @if (!$errors->has('password'))
+                            <input
+                                class="border border-gray-400 p-2 w-full rounded-md {{ $errors->has('password') ? 'border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500' : '' }}"
+                                name="password" id="password" type="password" placeholder="Enter your password" />
+                            <div class="absolute inset-y-0 right-0 pr-3 flex items-center">
+                                <button id="toggle-password" type="button" class=" focus:outline-none border-none">
+                                    <img src="{{ URL('img/hide.png') }}" class="h-6 w-6" id="imgClickAndChange"
+                                        onclick="changeImage()">
+                                    </img>
+                                </button>
+                            </div>
+                        @endif
+                        @if ($errors->has('password'))
+                            <input
+                                class="border border-gray-400 p-2 w-full rounded-md {{ $errors->has('password') ? 'border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500' : '' }}"
+                                name="password" id="passwordRed" type="password" placeholder="Enter your password" />
+                            <div class="absolute inset-y-0 right-0 pr-3 flex items-center">
+                                <button id="toggle-password-red" type="button" class=" focus:outline-none border-none">
+                                    <img src="{{ URL('img/hide_red.png') }}" class="h-6 w-6" id="imgClickAndChangeRed"
+                                        onclick="changeImageRed()">
+                                    </img>
+                                </button>
+                            </div>
+                        @endif
+                    </div>
+                    @if ($errors->has('password'))
+                        <p class="text-sm text-red-600 dark:text-red-500"><span
+                                class="font-medium">{{ $errors->first('password') }}</span></p>
+                    @endif
                 </div>
-                {{-- <div class="flex items-center justify-between mb-4">
-                    <label class="block text-gray-500 font-bold">
-                        <input class="mr-2 leading-tight" type="checkbox" />
-                        <span class="text-sm">
-                            Remember me
-                        </span>
-                    </label>
-                    <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                        Forgot Password?
-                    </a>
-                </div> --}}
                 <div>
                     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
                         type="submit">
@@ -66,6 +79,46 @@
             </form>
         </div>
     </div>
+    <script>
+        let image_tracker = 'view';
+
+        function changeImage() {
+            let image = document.getElementById('imgClickAndChange');
+            if (image_tracker == 'view') {
+                image.src = '/img/view.png';
+                image_tracker = 'hide';
+            } else {
+                image.src = '/img/hide.png';
+                image_tracker = 'view';
+            }
+        }
+
+        const togglePassword = document.querySelector('#toggle-password');
+        const password = document.querySelector('#password');
+
+        togglePassword.addEventListener('click', function(e) {
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+        });
+    </script>
+    <script>
+        function changeImageRed() {
+            let image = document.getElementById('imgClickAndChangeRed');
+            if (image_tracker == 'view') {
+                image.src = '/img/show_red.png';
+                image_tracker = 'hide';
+            } else {
+                image.src = '/img/hide_red.png';
+                image_tracker = 'view';
+            }
+        }
+        const passwordRed = document.querySelector('#passwordRed');
+        const togglePasswordRed = document.querySelector('#toggle-password-red');
+        togglePasswordRed.addEventListener('click', function(e) {
+            const type = passwordRed.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordRed.setAttribute('type', type);
+        });
+    </script>
     @vite('resources/js/app.js')
 </body>
 
